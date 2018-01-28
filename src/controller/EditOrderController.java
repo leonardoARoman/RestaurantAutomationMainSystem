6ef0c6fce40c1c4762a5226faeba58301a4be322
Leonardo Roman <leonardoroman@Leonardos-MacBook-Air.local>
2018-01-28 12:20:02 -0500

    edit order class model and view implemented

---
+++ src/controller/EditOrderController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class EditOrderController {
+	private static Stage stage;
+
+	public void start(Stage mainStage) {
+		stage = mainStage;
+	}
+}
