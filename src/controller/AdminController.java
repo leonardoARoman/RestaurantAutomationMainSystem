d6cdbc44bb2ff9be8aa67c15841ec31f5423fe4a
Leonardo Roman <leonardoroman@nwk-6-244.nwk.ruw.rutgers.edu>
2018-01-30 16:17:44 -0500

    new model and view files create

---
+++ src/controller/AdminController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class AdminController {
+	private static Stage stage;
+
+	public void start(Stage mainStage) {
+		stage = mainStage;
+	}
+}
---
+++ src/controller/ClockInTimeController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class ClockInTimeController {
+	private static Stage stage;
+
+	public void start(Stage mainStage) {
+		stage = mainStage;
+	}
+}
