c1bdf3d8b812d439e728c39a01bf316c4d5f22b7
Leonardo Roman <leonardoroman@Leonardos-Air.home>
2018-03-12 17:00:11 -0400

    socket communication using TCP protocol implemented

--- src/controller/FloorController.java
+++ src/controller/FloorController.java
@@ -1,6 +1,11 @@
 package controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import javafx.collections.FXCollections;
@@ -17,6 +22,11 @@ import javafx.stage.Stage;
 import model.MySqlManager;
 import model.TableState;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+
 /**
  * 
  * @author leonardoroman
@@ -29,6 +39,10 @@ public class FloorController {
 	private MySqlManager mysql;
 	private static Stage stage;
 	private ObservableList<String> specialsObsList,ffObsList,desertObsList,drinksObsList;
+	private static ServerSocket ss;
+	private static Socket socket;
+	private static DataInputStream dis;
+	private static DataOutputStream dos;
 
 	@FXML
 	private ImageView table1,table2,table3,table4,table5,table6,table7,table8,table9,table10,table11,table12;
@@ -51,8 +65,9 @@ public class FloorController {
 	/**
 	 * 
 	 * @param mainStage
+	 * @throws UnknownHostException 
 	 */
-	public void start(Stage mainStage) {
+	public void start(Stage mainStage) throws UnknownHostException {
 		stage = mainStage;
 		mysql = MySqlManager.getInstance();
 		specialsObsList = FXCollections.observableArrayList(mysql.getSpecials());
@@ -60,6 +75,11 @@ public class FloorController {
 		desertObsList = FXCollections.observableArrayList(mysql.getDesert());
 		ffObsList = FXCollections.observableArrayList(mysql.getFastFood());
 		showFastFood();
+
+		// New thread for listening to incoming msgs.
+		Thread t1 = new Thread(()->createServer());
+		t1.start();
+
 	}
 
 	/**
@@ -116,6 +136,13 @@ public class FloorController {
 			alert.setContentText("Reservation saved!");
 			alert.showAndWait();
 		}
+	}
+
+	// Here listen for waiter incoming table update
+	public void listeningPort() {
+		// need a thread to listen a port number continuously 
+
+		// get message and call the respective table function.
 	}
 
 	/**
@@ -766,6 +793,10 @@ public class FloorController {
 		}
 	}
 
+	/**
+	 * 
+	 * @throws IOException
+	 */
 	private void viewOrder() throws IOException {
 		// For management purposes.
 		FXMLLoader loader = new FXMLLoader();
@@ -811,5 +842,490 @@ public class FloorController {
 			return 4;
 		} 
 		return 0;
+	}
+
+	// Helper function to create server connection
+	private void createServer() {
+		ServerSocket serverSocket=null;
+		Socket clientSocket = null;
+		//create the port number
+		int port = 1234;
+
+		try {
+			//create the TCP socket server
+			serverSocket = new ServerSocket(port);
+			System.out.println("TCP server created on port = "+port);
+		} catch (IOException ex) {
+			//will be executed when the server cannot be created
+			System.out.println("Error: The server with port="+port+" cannot be created");
+		}
+
+		//starts an endless loop
+		while(true){
+			try {
+				//start listening to incoming client request (blocking function)
+				System.out.println("[TCP Server] waiting for the incoming request ...");
+				clientSocket = serverSocket.accept();
+
+			} catch (IOException e) {
+				System.out.println("Error: cannot accept client request. Exit program");
+				return;
+			}
+			try {
+				ArrayList<String> str = processClientRequest(clientSocket);
+				int tableNumber = Integer.parseInt(str.get(0));
+				int action = Integer.parseInt(str.get(1));
+				setTableState(tableNumber,action);
+			} catch (IOException e) {
+				//log exception and go on to next request.
+			}
+
+		}
+	}
+
+	private ArrayList<String> processClientRequest(Socket clientSocket) throws IOException {
+		
+		ArrayList<String> str = new ArrayList<String>();
+		
+		System.out.println("[TCP Server] processing the incoming request");
+		try {
+			PrintStream printStream = new PrintStream(clientSocket.getOutputStream());
+			InputStreamReader inputStream = new InputStreamReader(clientSocket.getInputStream());
+
+			//read the received message
+			BufferedReader bufferedReader = new BufferedReader(inputStream);
+			String message = null;
+			message = bufferedReader.readLine();
+			String[] code = message.split(" ");
+			str.add(code[0]);
+			str.add(code[1]);
+			System.out.println("message received from client: \n\t"+message);
+
+
+			//prepare the String. WARNING: add \n to make sure that the message is considered as one line
+			String messageSend = "Hello World From TCP Server!\n";
+			printStream.println(messageSend);
+			printStream.close();
+		}catch (Exception e){
+			System.out.print("[TCP Server] The server cannot send the message");
+		}
+		return str;
+	}
+
+	// Helper function to listen to table updates from mobile application
+	private void setTableState(int tableNumber, int action) throws IOException{
+		switch(tableNumber) {
+		case 1:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table1.setImage(TableState.ocupiedTable(url+"t1.png"));
+				floor1_table_1.setImage(TableState.ocupiedTable(url+"t1.png"));
+				return;
+			} else if(action == 2) {
+				table1.setImage(TableState.dirtyTable(url+"t1.png"));
+				floor1_table_1.setImage(TableState.dirtyTable(url+"t1.png"));
+				return;
+			} else if(action == 3) {
+				table1.setImage(TableState.cleanTable(url+"t1.png"));
+				floor1_table_1.setImage(TableState.cleanTable(url+"t1.png"));
+				return;
+			}
+		}
+		case 2:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table2.setImage(TableState.ocupiedTable(url+"t2.png"));
+				floor1_table_2.setImage(TableState.ocupiedTable(url+"t2.png"));
+				return;
+			} else if(action == 2) {
+				table2.setImage(TableState.dirtyTable(url+"t2.png"));
+				floor1_table_2.setImage(TableState.dirtyTable(url+"t2.png"));
+				return;
+			} else if(action == 3) {
+				table2.setImage(TableState.cleanTable(url+"t2.png"));
+				floor1_table_2.setImage(TableState.cleanTable(url+"t2.png"));
+				return;
+			}
+		}
+		case 3:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table3.setImage(TableState.ocupiedTable(url+"t3.png"));
+				floor2_table_1.setImage(TableState.ocupiedTable(url+"t3.png"));
+				return;
+			} else if(action == 2) {
+				table3.setImage(TableState.dirtyTable(url+"t3.png"));
+				floor2_table_1.setImage(TableState.dirtyTable(url+"t3.png"));
+				return;
+			} else if(action == 3) {
+				table3.setImage(TableState.cleanTable(url+"t3.png"));
+				floor2_table_1.setImage(TableState.cleanTable(url+"t3.png"));
+				return;
+			}
+		}
+		case 4:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table4.setImage(TableState.ocupiedTable(url+"t4.png"));
+				floor2_table_2.setImage(TableState.ocupiedTable(url+"t4.png"));
+				return;
+			} else if(action == 2) {
+				table4.setImage(TableState.dirtyTable(url+"t4.png"));
+				floor2_table_2.setImage(TableState.dirtyTable(url+"t4.png"));
+				return;
+			} else if(action == 3) {
+				table4.setImage(TableState.cleanTable(url+"t4.png"));
+				floor2_table_2.setImage(TableState.cleanTable(url+"t4.png"));
+				return;
+			}
+		}
+		case 5:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table5.setImage(TableState.ocupiedTable(url+"t5.png"));
+				floor1_table_3.setImage(TableState.ocupiedTable(url+"t5.png"));
+				return;
+			} else if(action == 2) {
+				table5.setImage(TableState.dirtyTable(url+"t5.png"));
+				floor1_table_3.setImage(TableState.dirtyTable(url+"t5.png"));
+				return;
+			} else if(action == 3) {
+				table5.setImage(TableState.cleanTable(url+"t5.png"));
+				floor1_table_3.setImage(TableState.cleanTable(url+"t5.png"));
+				return;
+			}
+		}
+		case 6:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table6.setImage(TableState.ocupiedTable(url+"t6.png"));
+				floor1_table_4.setImage(TableState.ocupiedTable(url+"t6.png"));
+				return;
+			} else if(action == 2) {
+				table6.setImage(TableState.dirtyTable(url+"t6.png"));
+				floor1_table_4.setImage(TableState.dirtyTable(url+"t6.png"));
+				return;
+			} else if(action == 3) {
+				table6.setImage(TableState.cleanTable(url+"t6.png"));
+				floor1_table_4.setImage(TableState.cleanTable(url+"t6.png"));
+				return;
+			}
+		}
+		case 7:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table7.setImage(TableState.ocupiedTable(url+"t7.png"));
+				floor2_table_3.setImage(TableState.ocupiedTable(url+"t7.png"));
+				return;
+			} else if(action == 2) {
+				table7.setImage(TableState.dirtyTable(url+"t7.png"));
+				floor2_table_3.setImage(TableState.dirtyTable(url+"t7.png"));
+				return;
+			} else if(action == 3) {
+				table7.setImage(TableState.cleanTable(url+"t7.png"));
+				floor2_table_3.setImage(TableState.cleanTable(url+"t7.png"));
+				return;
+			}
+		}
+		case 8:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table8.setImage(TableState.ocupiedTable(url+"t8.png"));
+				floor2_table_4.setImage(TableState.ocupiedTable(url+"t8.png"));
+				return;
+			} else if(action == 2) {
+				table8.setImage(TableState.dirtyTable(url+"t8.png"));
+				floor2_table_4.setImage(TableState.dirtyTable(url+"t8.png"));
+				return;
+			} else if(action == 3) {
+				table8.setImage(TableState.cleanTable(url+"t8.png"));
+				floor2_table_4.setImage(TableState.cleanTable(url+"t8.png"));
+				return;
+			}
+		}
+		case 9:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table9.setImage(TableState.ocupiedTable(url+"t9.png"));
+				floor1_table_5.setImage(TableState.ocupiedTable(url+"t9.png"));
+				return;
+			} else if(action == 2) {
+				table9.setImage(TableState.dirtyTable(url+"t9.png"));
+				floor1_table_5.setImage(TableState.dirtyTable(url+"t9.png"));
+				return;
+			} else if(action == 3) {
+				table9.setImage(TableState.cleanTable(url+"t9.png"));
+				floor1_table_5.setImage(TableState.cleanTable(url+"t9.png"));
+				return;
+			}
+		}
+		case 10:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table10.setImage(TableState.ocupiedTable(url+"t10.png"));
+				floor1_table_6.setImage(TableState.ocupiedTable(url+"t10.png"));
+				return;
+			} else if(action == 2) {
+				table10.setImage(TableState.dirtyTable(url+"t10.png"));
+				floor1_table_6.setImage(TableState.dirtyTable(url+"t10.png"));
+				return;
+			} else if(action == 3) {
+				table10.setImage(TableState.cleanTable(url+"t10.png"));
+				floor1_table_6.setImage(TableState.cleanTable(url+"t10.png"));
+				return;
+			}
+		}
+		case 11:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table11.setImage(TableState.ocupiedTable(url+"t11.png"));
+				floor2_table_5.setImage(TableState.ocupiedTable(url+"t11.png"));
+				return;
+			} else if(action == 2) {
+				table11.setImage(TableState.dirtyTable(url+"t11.png"));
+				floor2_table_5.setImage(TableState.dirtyTable(url+"t11.png"));
+				return;
+			} else if(action == 3) {
+				table11.setImage(TableState.cleanTable(url+"t11.png"));
+				floor2_table_5.setImage(TableState.cleanTable(url+"t11.png"));
+				return;
+			}
+		}
+		case 12:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table12.setImage(TableState.ocupiedTable(url+"t12.png"));
+				floor2_table_6.setImage(TableState.ocupiedTable(url+"t12.png"));
+				return;
+			} else if(action == 2) {
+				table12.setImage(TableState.dirtyTable(url+"t12.png"));
+				floor2_table_6.setImage(TableState.dirtyTable(url+"t12.png"));
+				return;
+			} else if(action == 3) {
+				table12.setImage(TableState.cleanTable(url+"t12.png"));
+				floor2_table_6.setImage(TableState.cleanTable(url+"t12.png"));
+				return;
+			}
+		}
+		case 13:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table20.setImage(TableState.ocupiedTable(url+"t20.png"));
+				floor3_table_1.setImage(TableState.ocupiedTable(url+"t20.png"));
+				return;
+			} else if(action == 2) {
+				table20.setImage(TableState.dirtyTable(url+"t20.png"));
+				floor3_table_1.setImage(TableState.dirtyTable(url+"t20.png"));
+				return;
+			} else if(action == 3) {
+				table20.setImage(TableState.cleanTable(url+"t20.png"));
+				floor3_table_1.setImage(TableState.cleanTable(url+"t20.png"));
+				return;
+			}
+		}
+		case 14:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table21.setImage(TableState.ocupiedTable(url+"t21.png"));
+				floor3_table_2.setImage(TableState.ocupiedTable(url+"t21.png"));
+				return;
+			} else if(action == 2) {
+				table21.setImage(TableState.dirtyTable(url+"t21.png"));
+				floor3_table_2.setImage(TableState.dirtyTable(url+"t21.png"));
+				return;
+			} else if(action == 3) {
+				table21.setImage(TableState.cleanTable(url+"t21.png"));
+				floor3_table_2.setImage(TableState.cleanTable(url+"t21.png"));
+				return;
+			} 
+		}
+		case 15:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table22.setImage(TableState.ocupiedTable(url+"t22.png"));
+				floor4_table_1.setImage(TableState.ocupiedTable(url+"t22.png"));
+				return;
+			} else if(action == 2) {
+				table22.setImage(TableState.dirtyTable(url+"t22.png"));
+				floor4_table_1.setImage(TableState.dirtyTable(url+"t22.png"));
+				return;
+			} else if(action == 3) {
+				table22.setImage(TableState.cleanTable(url+"t22.png"));
+				floor4_table_1.setImage(TableState.cleanTable(url+"t22.png"));
+				return;
+			}
+		}
+		case 16:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table23.setImage(TableState.ocupiedTable(url+"t23.png"));
+				floor4_table_2.setImage(TableState.ocupiedTable(url+"t23.png"));
+				return;
+			} else if(action == 2) {
+				table23.setImage(TableState.dirtyTable(url+"t23.png"));
+				floor4_table_2.setImage(TableState.dirtyTable(url+"t23.png"));
+				return;
+			} else if(action == 3) {
+				table23.setImage(TableState.cleanTable(url+"t23.png"));
+				floor4_table_2.setImage(TableState.cleanTable(url+"t23.png"));
+				return;
+			}
+		}
+		case 17:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table24.setImage(TableState.ocupiedTable(url+"t24.png"));
+				floor3_table_3.setImage(TableState.ocupiedTable(url+"t24.png"));
+				return;
+			} else if(action == 2) {
+				table24.setImage(TableState.dirtyTable(url+"t24.png"));
+				floor3_table_3.setImage(TableState.dirtyTable(url+"t24.png"));
+				return;
+			} else if(action == 3) {
+				table24.setImage(TableState.cleanTable(url+"t24.png"));
+				floor3_table_3.setImage(TableState.cleanTable(url+"t24.png"));
+				return;
+			}
+		}
+		case 18:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table25.setImage(TableState.ocupiedTable(url+"t25.png"));
+				floor3_table_4.setImage(TableState.ocupiedTable(url+"t25.png"));
+				return;
+			} else if(action == 2) {
+				table25.setImage(TableState.dirtyTable(url+"t25.png"));
+				floor3_table_4.setImage(TableState.dirtyTable(url+"t25.png"));
+				return;
+			} else if(action == 3) {
+				table25.setImage(TableState.cleanTable(url+"t25.png"));
+				floor3_table_4.setImage(TableState.cleanTable(url+"t25.png"));
+				return;
+			} 
+		}
+		case 19:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table26.setImage(TableState.ocupiedTable(url+"t26.png"));
+				floor4_table_3.setImage(TableState.ocupiedTable(url+"t26.png"));
+				return;
+			} else if(action == 2) {
+				table26.setImage(TableState.dirtyTable(url+"t26.png"));
+				floor4_table_3.setImage(TableState.dirtyTable(url+"t26.png"));
+				return;
+			} else if(action == 3) {
+				table26.setImage(TableState.cleanTable(url+"t26.png"));
+				floor4_table_3.setImage(TableState.cleanTable(url+"t26.png"));
+				return;
+			}
+		}
+		case 20:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table27.setImage(TableState.ocupiedTable(url+"t27.png"));
+				floor4_table_4.setImage(TableState.ocupiedTable(url+"t27.png"));
+				return;
+			} else if(action == 2) {
+				table27.setImage(TableState.dirtyTable(url+"t27.png"));
+				floor4_table_4.setImage(TableState.dirtyTable(url+"t27.png"));
+				return;
+			} else if(action == 3) {
+				table27.setImage(TableState.cleanTable(url+"t27.png"));
+				floor4_table_4.setImage(TableState.cleanTable(url+"t27.png"));
+				return;
+			}
+		}
+		case 21:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table28.setImage(TableState.ocupiedTable(url+"t28.png"));
+				floor3_table_5.setImage(TableState.ocupiedTable(url+"t28.png"));
+				return;
+			} else if(action == 2) {
+				table28.setImage(TableState.dirtyTable(url+"t28.png"));
+				floor3_table_5.setImage(TableState.dirtyTable(url+"t28.png"));
+				return;
+			} else if(action == 3) {
+				table28.setImage(TableState.cleanTable(url+"t28.png"));
+				floor3_table_5.setImage(TableState.cleanTable(url+"t28.png"));
+				return;
+			}
+		}
+		case 22:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table29.setImage(TableState.ocupiedTable(url+"t29.png"));
+				floor3_table_6.setImage(TableState.ocupiedTable(url+"t29.png"));
+				return;
+			} else if(action == 2) {
+				table29.setImage(TableState.dirtyTable(url+"t29.png"));
+				floor3_table_6.setImage(TableState.dirtyTable(url+"t29.png"));
+				return;
+			} else if(action == 3) {
+				table29.setImage(TableState.cleanTable(url+"t29.png"));
+				floor3_table_6.setImage(TableState.cleanTable(url+"t29.png"));
+				return;
+			}
+		}
+		case 23:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table30.setImage(TableState.ocupiedTable(url+"t30.png"));
+				floor4_table_5.setImage(TableState.ocupiedTable(url+"t30.png"));
+				return;
+			} else if(action == 2) {
+				table30.setImage(TableState.dirtyTable(url+"t30.png"));
+				floor4_table_5.setImage(TableState.dirtyTable(url+"t30.png"));
+				return;
+			} else if(action == 3) {
+				table30.setImage(TableState.cleanTable(url+"t30.png"));
+				floor4_table_5.setImage(TableState.cleanTable(url+"t30.png"));
+				return;
+			}
+		}case 24:{
+			if(action == 0) {
+				return;
+			}else if(action == 1) {
+				table31.setImage(TableState.ocupiedTable(url+"t31.png"));
+				floor4_table_6.setImage(TableState.ocupiedTable(url+"t31.png"));
+				return;
+			} else if(action == 2) {
+				table31.setImage(TableState.dirtyTable(url+"t31.png"));
+				floor4_table_6.setImage(TableState.dirtyTable(url+"t31.png"));
+				return;
+			} else if(action == 3) {
+				table31.setImage(TableState.cleanTable(url+"t31.png"));
+				floor4_table_6.setImage(TableState.cleanTable(url+"t31.png"));
+				return;
+			}
+		}
+		default:
+			if(action < 0 || action > 3)
+				return;
+		}
+
 	}
 }
